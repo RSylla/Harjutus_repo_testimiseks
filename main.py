93d18aa2a31740425fe11866e2c63a5fd0e831df
RSylla <103101146+RSylla@users.noreply.github.com>
2022-11-24 17:09:43 +0200

    Added functionality of sorting persons into groups

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 
 class Person():
@@ -13,25 +14,57 @@ class Person():
     def __str__(self):
         return f"{self.__first_name}, {self.last_name}, {self.age}"
 
-
-if __name__ == "__main__":
-
-    csv_path = input("Path to CSV file >>> ")
+def data_from_csv_to_Person_instances(csv_path):
     file = open(csv_path, "r", encoding="utf-8")
     persons_list = []
     with file as f:
         keys = [i for i in f.readline().strip().split(",")]
-
         for line in f.readlines():
             values = [i for i in line.strip().split(",")]
             dicty = {}
             for i in range(len(keys)):
                 dicty[keys[i]] = values[i]
-
             persons_list.append(Person(**dicty))
-            
-    for item in persons_list:
-        print(item)
+    print(f"Added {len(persons_list)} persons form csv file.")
+    return persons_list
+
+
+
+if __name__ == "__main__":
+
+    csv_path = input("Path to CSV file >>> ")
+    persons_list = data_from_csv_to_Person_instances(csv_path)
+    max_persons = int(input("How many persons in each group? >>> "))
+
+    def divide_persons_into_group(persons_list, max_persons):
+        groups = []
+
+        num_of_groups = len(persons_list) / max_persons \
+            if len(persons_list) % max_persons == 0 \
+            else (len(persons_list) // max_persons) + 1
+
+
+        for i in range(int(num_of_groups)):
+            lst = []
+            if len(persons_list) % max_persons != 0 and len(persons_list) < (2*max_persons):
+                max_persons = max_persons - 1
+
+            for i in range(max_persons):
+                try:
+                    person = random.choice(persons_list)
+                    lst.append(person)
+                    persons_list.remove(person)
+                except IndexError:
+                    continue
+
+            groups.append(lst)
+        return groups
+
+    for item in divide_persons_into_group(persons_list, max_persons):
+        for i in item:
+            print(i)
+        print("--------------------")
+
 
 
 
