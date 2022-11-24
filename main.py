72ff3bd0b519f9499ac58cbe5f9033d7e33f831d
RSylla <103101146+RSylla@users.noreply.github.com>
2022-11-24 14:31:44 +0200

    Added functionality to read data from csv, convert data into list of Person class instances and edited Person class

--- main.py
+++ main.py
@@ -9,3 +9,29 @@ class Person():
 
     def get_first_name(self):
         return self.__first_name
+
+    def __str__(self):
+        return f"{self.__first_name}, {self.last_name}, {self.age}"
+
+
+if __name__ == "__main__":
+
+    csv_path = input("Path to CSV file >>> ")
+    file = open(csv_path, "r", encoding="utf-8")
+    persons_list = []
+    with file as f:
+        keys = [i for i in f.readline().strip().split(",")]
+
+        for line in f.readlines():
+            values = [i for i in line.strip().split(",")]
+            dicty = {}
+            for i in range(len(keys)):
+                dicty[keys[i]] = values[i]
+
+            persons_list.append(Person(**dicty))
+            
+    for item in persons_list:
+        print(item)
+
+
+
